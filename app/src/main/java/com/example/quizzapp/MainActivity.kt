9ac430080c4bbbfc1bf7a7fad44f6ebc772b2943
pramod <pramodpal033@gmail.com>
2022-09-21 08:56:32 +0530

    update Pramod Laptop

--- app/src/main/java/com/example/quizzapp/MainActivity.kt
+++ app/src/main/java/com/example/quizzapp/MainActivity.kt
@@ -5,18 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val nextBtn = findViewById<Button>(R.id.nextBtn)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this@MainActivity,WelcomePage::class.java)
-            startActivity(intent)
+        nextBtn.setOnClickListener({
+            val intent = Intent(this@MainActivity,Questions::class.java)
+        startActivity(intent)
+            finish()
+        })
 
-        }, 3000)
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            val intent = Intent(this@MainActivity,WelcomePage::class.java)
+//            startActivity(intent)
+//
+//        }, 3000)
 
     }
 }
