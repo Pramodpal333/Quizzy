c01162cfb46ad3f8a892450f570b4174a0b039c5
pramod <pramodpal033@gmail.com>
2022-09-21 08:37:03 +0530

    update Pramod Laptop

--- app/src/main/java/com/example/quizzapp/MainActivity.kt
+++ app/src/main/java/com/example/quizzapp/MainActivity.kt
@@ -4,17 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Handler().postDelayed({
-            val intent = Intent(this,WelcomePage::class.java)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@MainActivity,WelcomePage::class.java)
             startActivity(intent)
 
-        }, 2000)
+        }, 3000)
 
     }
 }
